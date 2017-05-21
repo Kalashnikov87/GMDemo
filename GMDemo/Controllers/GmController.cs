fcd8f9b64adef8eeabb10ede54a600c495e096f8
JP Myburgh <jp.myburgh@mint.co.za>
2017-05-21 20:26:27 +0200

    WIP - initial commit

---
+++ GMDemo/Controllers/GmController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GMDemo.Controllers
+{
+    public class GmController : Controller
+    {
+        // GET: Gm
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GMDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GMDemo.MvcApplication" Language="C#" %>
